bd9657fa05dd46b9e0d2cf24b86b1336c24e93bc
mogana varshini <92911280+Mogana004@users.noreply.github.com>
2023-07-17 09:08:10 +0530

    Update odd.java

--- odd.java
+++ odd.java
@@ -2,13 +2,10 @@ import java.io.*;
 import  java.util.*;
 class UserMainCode
 {
- public int secondLastDigitOf(int input1){
-  if(input1>=-9 && input1<=9)
-   return -1;
-   if(input1<0)
-   input1*=-1;
-  input1/=10;
-  int n=input1%10;
-  return n;
+ public int isEven(int input1){
+  if(input1%2!=0)
+   return 2;
+  else
+   return 1;
  }
 }
